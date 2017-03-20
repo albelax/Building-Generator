0b26f61e2b6816a0d95a51c0fd7b711972b064d9
Alberto La Scala <albelax94@gmail.com>
2017-03-20 10:19:41 +0000

    added multiple buildings to the scene

--- Building-Generator/src/main.cpp
+++ Building-Generator/src/main.cpp
@@ -70,22 +70,40 @@ int main()
 	MixBuilder mixBuilder = MixBuilder((random()%3)+2);
 	Building mixBuilding = mixBuilder.getBuilding();
 
+	WindowsOnlyBuilder windowsOnlyBuiler = WindowsOnlyBuilder((random()%3)+2);
+	Building windowsOnly = windowsOnlyBuiler.getBuilding();
+
 	MixBuilder mixBuilder2 = MixBuilder((random()%3)+2);
 	Building mixBuilding2 = mixBuilder2.getBuilding();
 
-	int bufferSize = mixBuilding.amountVertices() + mixBuilding2.amountVertices();
+	std::vector<Building> buildings;
+	buildings.push_back(mixBuilding);
+	buildings.push_back(mixBuilding2);
+	buildings.push_back(windowsOnly);
+
+	int bufferSize = 0;
+	for (auto building : buildings)
+	{
+		bufferSize += building.amountVertices();
+	}
+
 
 	Buffer buffer(bufferSize, sizeof(float)); // generate vbo buffer
-	mixBuilding.setBufferIndex(buffer.append((void *) mixBuilding.getVertices(), mixBuilding.amountVertices(), Buffer::VERTEX));
-	mixBuilding2.setBufferIndex(buffer.append((void *) mixBuilding2.getVertices(), mixBuilding2.amountVertices(), Buffer::VERTEX));
+
+	for (auto &building : buildings)
+	{
+		building.setBufferIndex(buffer.append((void *) building.getVertices(), building.amountVertices(), Buffer::VERTEX));
+	}
 
 	// pass the vertex data to the shader
 	GLint pos = glGetAttribLocation(test.getShaderProgram(), "VertexPosition");
 	glEnableVertexAttribArray(pos);
 	glVertexAttribPointer(pos, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-	buffer.append((void *) mixBuilding.getNormals(), mixBuilding.amountVertices(), Buffer::NORMAL);
-	buffer.append((void *) mixBuilding2.getNormals(), mixBuilding2.amountVertices(), Buffer::NORMAL);
+	for (auto &building : buildings)
+	{
+		buffer.append((void *) building.getNormals(), building.amountVertices(), Buffer::NORMAL);
+	}
 
 	// pass the normals to the shader
 	GLint n = glGetAttribLocation(test.getShaderProgram(), "VertexNormal");
@@ -105,7 +123,7 @@ int main()
 	TrackballCamera mainCamera = TrackballCamera();
 	mainCamera.setInitialMousePos(0,0);
 	mainCamera.setTarget(0.0f,0.0f,0.0f);
-	mainCamera.setEye(0.0f, 2.0f, 5.0f);
+	mainCamera.setEye(-6.0f, 4.0f, 0.0f);
 
 	SDL_Event event;
 	bool quit = false;
@@ -149,9 +167,29 @@ int main()
 					quit = true;
 					break;
 				}
-				case SDLK_s:
+				case SDLK_1:
 				{
-					Mesh::write(mixBuilding.getVerticesContainer(), mixBuilding.getNormalsContainer(), "out.obj");
+					if (buildings.size() > 0)
+					{
+						Mesh::write(buildings[0].getVerticesContainer(), buildings[0].getNormalsContainer(), "1_out.obj");
+					}
+					break;
+				}
+
+				case SDLK_2:
+				{
+					if (buildings.size() > 1)
+					{
+						Mesh::write(buildings[1].getVerticesContainer(), buildings[1].getNormalsContainer(), "2_out.obj");
+					}
+					break;
+				}
+				case SDLK_3:
+				{
+					if (buildings.size() > 2)
+					{
+						Mesh::write(buildings[2].getVerticesContainer(), buildings[2].getNormalsContainer(), "3_out.obj");
+					}
 					break;
 				}
 				default:break;
@@ -162,21 +200,20 @@ int main()
 		glClearColor(0.36f,0.36f,0.36f,1.0f);
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		glm::mat4 MV = glm::mat4(1.0);
-		MVP = projection * mainCamera.viewMatrix() * MV;
-		N = glm::mat3(glm::inverse(glm::transpose(MV)));
-		glUniformMatrix4fv(MVP_address, 1, GL_FALSE, glm::value_ptr(MVP));
-		glUniformMatrix4fv(MV_address, 1, GL_FALSE, glm::value_ptr(MV));
-		glUniformMatrix3fv(N_address, 1, GL_FALSE, glm::value_ptr(N));
-		glDrawArrays(GL_TRIANGLES, mixBuilding.getBufferIndex()/3, mixBuilding.amountVertices()/3);
 
-		MV = glm::translate(MV, glm::vec3(0,0,5.5f));
-		MVP = projection * mainCamera.viewMatrix() * MV;
-		N = glm::mat3(glm::inverse(glm::transpose(MV)));
-		glUniformMatrix4fv(MVP_address, 1, GL_FALSE, glm::value_ptr(MVP));
-		glUniformMatrix4fv(MV_address, 1, GL_FALSE, glm::value_ptr(MV));
-		glUniformMatrix3fv(N_address, 1, GL_FALSE, glm::value_ptr(N));
-		glDrawArrays(GL_TRIANGLES, mixBuilding2.getBufferIndex()/3, mixBuilding2.amountVertices()/3);
+		glm::vec3 offset = glm::vec3(0,0,7);
+		glm::mat4 MV = glm::mat4(1.0);
+		MV = glm::translate(MV, -offset*buildings.size()/2);
+		for (auto &building : buildings)
+		{
+			MV = glm::translate(MV, offset);
+			MVP = projection * mainCamera.viewMatrix() * MV;
+			N = glm::mat3(glm::inverse(glm::transpose(MV)));
+			glUniformMatrix4fv(MVP_address, 1, GL_FALSE, glm::value_ptr(MVP));
+			glUniformMatrix4fv(MV_address, 1, GL_FALSE, glm::value_ptr(MV));
+			glUniformMatrix3fv(N_address, 1, GL_FALSE, glm::value_ptr(N));
+			glDrawArrays(GL_TRIANGLES, building.getBufferIndex()/3, building.amountVertices()/3);
+		}
 
 		SDL_GL_SwapWindow(mainWindow.getWindow());
 	}
